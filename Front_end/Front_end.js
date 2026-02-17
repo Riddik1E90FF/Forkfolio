79f917558bfcf4be6e18773444dbd7a30ddf8bd3
Littlebeanburrito <coronajonathan885@gmail.com>
2026-02-16 19:50:20 -0700

    finished most of the front end

---
+++ Front_end/Front_end.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const port = 7000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/recipe', (req, res) => {
+  res.render('recipe');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
